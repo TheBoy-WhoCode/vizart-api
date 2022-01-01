6cf24c6a8bdfa3838779d2c24587f640cc168743
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-01-01 14:38:13 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,295 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DECIMAL, TIMESTAMP
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean, Date
+
+'''TRIAL IMAGE'''
+
+
+class TrialImage(Base):
+    __tablename__ = "trial_image"
+
+    id = Column(String, primary_key=True, nullable=False)
+    image_path = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+
+
+'''TOKENS'''
+
+
+class Tokens(Base):
+    __tablename__ = "tokens"
+
+    id = Column(String, primary_key=True, nullable=False)
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    access_token = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+
+
+'''USER'''
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+
+
+'''USER PAYEMENT'''
+
+
+class UserPayments(Base):
+    __tablename__ = "user_payments"
+
+    id = Column(String, primary_key=True, nullable=False)
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    payment_type = Column(String, nullable=False)
+    expiry = Column(Date,
+                    nullable=False, server_default=text("now()"))
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+
+
+'''USER ADDRESS'''
+
+
+class UserAddress(Base):
+    __tablename__ = "user_address"
+
+    id = Column(String, primary_key=True, nullable=False)
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    address = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    pin_code = Column(Integer, nullable=False)
+    mobile = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+
+
+'''UPLOAD IMAGE'''
+
+
+class UploadImage(Base):
+    __tablename__ = "upload_image"
+
+    id = Column(String, primary_key=True, nullable=False)
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    image_path = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+
+
+'''PAYMENT DETAILS'''
+
+
+class PaymentDetails(Base):
+    __tablename__ = "payment_details"
+
+    id = Column(String, primary_key=True, nullable=False)
+    amount = Column(DECIMAL, nullable=False)
+    status = Column(Boolean, nullable=False, server_default="False")
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+
+
+'''ORDER DETAILS'''
+
+
+class OrderDetails(Base):
+    __tablename__ = "order_details"
+
+    id = Column(String, primary_key=True, nullable=False)
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    payment_id = Column(
+        String, ForeignKey("payment_details.id", ondelete="CASCADE"), nullable=False
+    )
+    total = Column(DECIMAL, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+    payment_details = relationship("PaymentDetails")
+
+
+'''PRODUCT CATEGORY'''
+
+
+class ProductCategory(Base):
+    __tablename__ = "product_category"
+
+    id = Column(String, primary_key=True, nullable=False)
+    category_name = Column(String, nullable=False)
+    category_desc = Column(String, nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    deleted_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+
+
+'''PRODUCT INVENTORY'''
+
+
+class ProductInventory(Base):
+    __tablename__ = "product_inventory"
+
+    id = Column(String, primary_key=True, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+
+
+'''PRODUCTS'''
+
+
+class Prodcuts(Base):
+    __tablename__ = "products"
+
+    id = Column(String, primary_key=True, nullable=False)
+    inventory_id = Column(
+        String, ForeignKey("product_inventory.id", ondelete="CASCADE"), nullable=False, unique=True
+    )
+    category_id = Column(
+        String, ForeignKey("product_category.id", ondelete="CASCADE"), nullable=False, unique=True
+    )
+    product_name = Column(String, nullable=False)
+    product_type = Column(String, nullable=False)
+    price = Column(DECIMAL, nullable=False)
+    product_image = Column(String, nullable=False)
+    product_desc = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    product_inventory = relationship("ProductInventory")
+    product_category = relationship('ProductCategory')
+
+
+'''PRODUCT IMAGE'''
+
+
+class ProductImage(Base):
+    __tablename__ = "product_image"
+
+    id = Column(String, primary_key=True, nullable=False)
+    product_id = Column(
+        String, ForeignKey("products.id", ondelete="CASCADE"), nullable=False
+    )
+    inventory_id = Column(
+        String, ForeignKey("products.inventory_id", ondelete="CASCADE"), nullable=False
+    )
+    category_id = Column(
+        String, ForeignKey("products.category_id", ondelete="CASCADE"), nullable=False
+    )
+    image_path = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    products = relationship("Products")
+
+
+'''ORDER ITEMS'''
+
+
+class OrderItems(Base):
+    __tablename__ = "order_items"
+
+    id = Column(String, primary_key=True, nullable=False)
+    product_id = Column(
+        String, ForeignKey("products.id", ondelete="CASCADE"), nullable=False
+    )
+    inventory_id = Column(
+        String, ForeignKey("products.inventory_id", ondelete="CASCADE"), nullable=False
+    )
+    category_id = Column(
+        String, ForeignKey("products.category_id", ondelete="CASCADE"), nullable=False
+    )
+    order_id = Column(
+        String, ForeignKey("order_details.id", ondelete="CASCADE"), nullable=False
+    )
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    payment_id = Column(
+        String, ForeignKey("payment_details.id", ondelete="CASCADE"), nullable=False
+    )
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+    payment_details = relationship("PaymentDetails")
+    products = relationship("Products")
+    order_details = relationship('OrderDetails')
+
+
+'''CART'''
+
+
+class Cart(Base):
+    __tablename__ = "cart"
+
+    id = Column(String, primary_key=True, nullable=False)
+    user_id = Column(
+        String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    product_id = Column(
+        String, ForeignKey("products.id", ondelete="CASCADE"), nullable=False
+    )
+    inventory_id = Column(
+        String, ForeignKey("products.inventory_id", ondelete="CASCADE"), nullable=False
+    )
+    category_id = Column(
+        String, ForeignKey("products.category_id", ondelete="CASCADE"), nullable=False
+    )
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text("now()"))
+    user = relationship("User")
+    products = relationship("Products")
---
+++ app/oauth2.py
@@ -0,0 +1,46 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm.session import Session
+from sqlalchemy.sql.functions import mode
+from app import models, schemas
+from .database import get_db
+from .config import settings
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+def verify_access_token(token: str, credential_exception):
+    try:
+        pay_load = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = pay_load.get("user_id")
+
+        if id is None:
+            raise credential_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credential_exception
+
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                         detail=f"Could not validate credentials",
+                                         headers={"WWW-Authenticate": "Bearer"})
+    token = verify_access_token(token, credential_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
