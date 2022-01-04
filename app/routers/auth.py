fed81829774127121b6243c272294fb0c495dd87
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-01-04 15:04:54 +0530

    login issue finally solved

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,11 +12,12 @@ router = APIRouter(
 )
 
 
-@router.post("/login",  response_model=schemas.Token, status_code=status.HTTP_200_OK)
+@router.post("/login",  status_code=status.HTTP_200_OK)
 async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.Users).filter(
         models.Users.email == user_credentials.username).first()
-    token_query = db.query(models.Tokens).filter(models.Users.id == user.id)
+    token_query = db.query(models.Tokens).filter(
+        models.Tokens.user_id == user.id)
     token = token_query.first()
 
     if not user:
@@ -30,16 +31,17 @@ async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Ses
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     values = models.Tokens(id=str(uuid.uuid1()),
                            user_id=user.id, access_token=access_token)
+
     if token is None:
+        print("[INFO] Inside IF")
         db.add(values)
         db.commit()
         db.refresh(values)
-    elif user.id == token.user_id and token.user_id is not None:
-        token_query.update({"access_token": access_token,
-                           "updated_at": datetime.now()}, synchronize_session=False)
-        db.commit()
     else:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Will Resolve it soon")
+        print("[INFO] Inside Else")
+        token_query.update({"access_token": access_token, "updated_at": datetime.now()},
+                           synchronize_session=False)
+        db.commit()
 
-    return {"status": "success", "access_token": access_token}
+    return_value = {"access_token": access_token, "status": "success"}
+    return return_value
